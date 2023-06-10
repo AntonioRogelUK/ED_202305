562ebe95514f9eb258daeaba1002e856e6570eb3
Edgar Renato Pastrana Alvarez <edgarpastrana95@gmail.com>
2023-06-10 10:54:01 -0700

    Agregando pilas en el proyecto de arreglos

--- Arreglos/Pilas.cs
+++ Arreglos/Pilas.cs
@@ -1,19 +1,35 @@
 ﻿namespace Arreglos
 {
-    internal class Pilas 
+    public class Pilas
     {
         private string[] arreglo;
         private int max;
         private int tope;
 
-        public Pilas (int elementos)
+        public Pilas(int elementos)
         {
-            arreglo = new string[elementos]
-                tope = 0;
-            max = arreglo.length - 1;
+            arreglo = new string[elementos];
+            tope = 0;
+            max = arreglo.Length - 1;
         }
+        public bool EsVacio()
+        {
+            return tope < 1;
+        }
+        public bool EstaLleno()
 
+        {
+            return tope > max;
+        }
+        public void Agregar(string dato)
+        {
+            if (EstaLleno())
+            {
+                throw new Exception("El arreglo esta lleno");
+            }
+            arreglo[tope] = dato;
+            tope++;
 
-
+        }
     }
 }
