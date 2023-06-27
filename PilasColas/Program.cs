40b4a11f4b4fe09fbf26ee7fe1cc18bcfd367bb6
David cervantes <xRaptor_XD@hotmail.com>
2023-06-26 20:36:08 -0700

    actualizar

--- PilasColas/Program.cs
+++ PilasColas/Program.cs
@@ -1,38 +1,36 @@
-﻿using System.Threading.Channels;
+﻿
+try
+{
 
 
+    Arreglos.Colas colas = new Arreglos.Colas(10);
+    colas.Agregar("A");
+    colas.Agregar("B");
+    colas.Agregar("C");
+    colas.Agregar("D");
+    string contenido = colas.Imprimir();
+    Console.WriteLine(contenido);
+    Console.WriteLine(new string('-', 10));
+    colas.Eliminar();
+    Console.WriteLine(colas.Imprimir());
+    Console.WriteLine(new string('-', 10));
+    colas.Eliminar();
+    Console.WriteLine(colas.Imprimir());
+    Console.WriteLine(new string('-', 10));
+    colas.Eliminar();
+    Console.WriteLine(colas.Imprimir());
+    Console.WriteLine(new string('-', 10));
+    colas.Eliminar();
+    Console.WriteLine(colas.Imprimir());
+
+    listas
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Ocurrió un error:{ex.Message}");
 
 
-//try
-//{
-
-
-//    Arreglos.Colas colas = new Arreglos.Colas(10);
-//    colas.Agregar("A");
-//    colas.Agregar("B");
-//    colas.Agregar("C");
-//    colas.Agregar("D");
-//    string contenido = colas.Imprimir();
-//    Console.WriteLine(contenido);
-//    Console.WriteLine(new string('-',10));
-//    colas.Eliminar();
-//    Console.WriteLine(colas.Imprimir());
-//    Console.WriteLine(new string('-', 10));
-//    colas.Eliminar();
-//    Console.WriteLine(colas.Imprimir());
-//    Console.WriteLine(new string('-', 10));
-//    colas.Eliminar();
-//    Console.WriteLine(colas.Imprimir());
-//    Console.WriteLine(new string('-', 10));
-//    colas.Eliminar();
-//    Console.WriteLine(colas.Imprimir());
-
-//}
-//catch (Exception ex) 
-//{
-
-//    Console.WriteLine($"Ocurrió un error:{ex.Message}"); 
-
-
-//}
+}
 
